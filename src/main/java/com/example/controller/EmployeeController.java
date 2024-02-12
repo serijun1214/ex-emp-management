b0018e0a1037474ce2bfa54e0b70ca183749f139
serijun1214 <junki.seri@rakus-partners.co.jp>
2024-02-13 00:45:03 +0900

    EmployeeControllerクラスの定義

---
+++ src/main/java/com/example/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package com.example.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.domain.Employee;
+import com.example.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+  
+  @Autowired
+  private EmployeeService employeeService;
+
+  @GetMapping("/showList")
+  public String showList(Model model) {
+    List<Employee> employeeList = employeeService.showList();
+    model.addAttribute("employeeList", employeeList);
+    return "employee/list";
+  }
+}
