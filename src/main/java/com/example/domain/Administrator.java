8f2ff37b011b2b3efa89fd40d04c89e04d9ff796
serijun1214 <junki.seri@rakus-partners.co.jp>
2024-02-12 21:50:30 +0900

    Employeeクラスの作成

--- src/main/java/com/example/domain/Administrator.java
+++ src/main/java/com/example/domain/Administrator.java
@@ -1,42 +1,54 @@
 package com.example.domain;
 
 /**
- * Administratorテーブルを表すクラス
+ * 管理者情報を表すドメインクラス
  */
 public class Administrator {
-  //ID
+  // ID
   private Integer id;
-  //名前
+  // 名前
   private String name;
-  //メールアドレス
+  // メールアドレス
   private String mailAddress;
-  //パスワード
+  // パスワード
   private String password;
+
+  // コンストラクタ
+  public Administrator() {
+  }
 
   public Integer getId() {
     return id;
   }
+
   public void setId(Integer id) {
     this.id = id;
   }
+
   public String getName() {
     return name;
   }
+
   public void setName(String name) {
     this.name = name;
   }
+
   public String getMailAddress() {
     return mailAddress;
   }
+
   public void setMailAddress(String mailAddress) {
     this.mailAddress = mailAddress;
   }
+
   public String getPassword() {
     return password;
   }
+
   public void setPassword(String password) {
     this.password = password;
   }
+
   @Override
   public String toString() {
     return "Administrator [id=" + id + ", name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
