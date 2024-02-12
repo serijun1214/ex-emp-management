9375120cdbfd7682b09b5ca558ea52316d7065fa
serijun1214 <junki.seri@rakus-partners.co.jp>
2024-02-12 22:45:23 +0900

    AdministratorRepositoryクラスの作成

---
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -0,0 +1,79 @@
+package com.example.repository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import com.example.domain.Administrator;
+
+/**
+ * 管理者情報を操作するリポジトリ.
+ */
+@Repository
+public class AdministratorRepository {
+
+  // NamedParameterJdbcTemplateをDIする
+  @Autowired
+  NamedParameterJdbcTemplate template;
+
+  // RowMapper
+  private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER = (rs, i) -> {
+    Administrator administrator = new Administrator();
+    administrator.setId(rs.getInt("id"));
+    administrator.setName(rs.getString("name"));
+    administrator.setMailAddress(rs.getString("mail_address"));
+    administrator.setPassword(rs.getString("password"));
+    return administrator;
+  };
+
+  /**
+   * 管理者情報を挿入する
+   * 
+   * @param administrator 管理者情報
+   */
+
+  public void insert(Administrator administrator) {
+    // SQL文を作成
+    String sql = "INSERT INTO administrators (name, mail_address, password) VALUES (:name, :mailAddress, :password)";
+
+    // パラメータを作成
+    SqlParameterSource param = new MapSqlParameterSource()
+    .addValue("name", administrator.getName())
+    .addValue("mailAddress", administrator.getMailAddress())
+    .addValue("password", administrator.getPassword());
+
+    // SQLを実行
+    template.update(sql, param);
+  }
+
+  /**
+   * 管理者情報を取得する
+   * 
+   * @param maillAddress メールアドレス
+   * @param password     パスワード
+   * @return Administrator 管理者情報
+   */
+  public Administrator findByMailAddressAndPassword(String maillAddress, String password) {
+    // SQL文を作成
+    String sql = "SELECT id, name, mail_address, password FROM administrators WHERE mail_address = :mailAddress AND password = :password";
+
+    // パラメータを作成
+    SqlParameterSource param = new MapSqlParameterSource()
+    .addValue("mailAddress", maillAddress)
+    .addValue("password",password);
+
+    // SQLを実行
+    List<Administrator> administratorList = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
+    // 結果がなければnullを返す
+    if (administratorList.size() == 0) {
+      return null;
+    }
+    // 結果があれば最初の要素を返す
+    return administratorList.get(0);
+  }
+}
